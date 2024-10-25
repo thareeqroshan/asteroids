f60d15330d66a11f13036b0713e229da6140f836
thareeqroshan <thareeqroshan@gmail.com>
2024-10-25 20:04:01 +0200

    Added Player

--- main.py
+++ main.py
@@ -1,18 +1,33 @@
 import pygame
 from constants import *
- 
+from player import Player 
 def main():
 	print("Starting asteroids!")
 	print("Screen width: " + str(SCREEN_WIDTH))
 	print("Screen height: " + str(SCREEN_HEIGHT))
 	pygame.init()
+	clock = pygame.time.Clock()
+	dt = 0
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	x = SCREEN_WIDTH/2
+	y = SCREEN_HEIGHT/2
+	updatable = pygame.sprite.Group()
+	drawable = pygame.sprite.Group()
+	Player.containers = (updatable, drawable)
+	player = Player(x, y)
 	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
 		screen.fill((0,0,0))
+		
+		for object in updatable:
+			object.update(dt)
+		
+		for object in  drawable:
+			object.draw(screen)
 		pygame.display.flip()
+		dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
 	main()
